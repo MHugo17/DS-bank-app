724e89540826ce09fd9a5841304d44c15a82bcb1
Martin Hugo <hugo0811@gmail.com>
2019-02-05 16:59:14 +0100

    Bank Account Test

---
+++ src/app/bank.py
@@ -0,0 +1,16 @@
+class Bank:
+    def __init__(self, name):
+        self.name = name
+        self.accounts = []
+        self.transactions = []
+
+    def open_account(self, account):
+        self.accounts.append(account)
+        return account
+
+    def add_transaction(self, *, sender, recipient, subject, amount):
+        assert amount > 0,'Amount has to be greater than 0'
+        transaction = {'sender':sender, 'recipient':recipient, 'subject':subject, 'amount':amount}
+        self.transactions.append(transaction)
+        return transaction
+
