d84b024fae0b9724b4eb39e976332b6d6e570d89
Martin Hugo <hugo0811@gmail.com>
2019-02-06 10:50:05 +0100

    syntax errors

--- src/app/bank.py
+++ src/app/bank.py
@@ -1,16 +1,19 @@
 class Bank:
+
+    # initialize
     def __init__(self, name):
         self.name = name
         self.accounts = []
         self.transactions = []
 
+    # open account
     def open_account(self, account):
         self.accounts.append(account)
         return account
 
+    # add transaction
     def add_transaction(self, *, sender, recipient, subject, amount):
-        assert amount > 0,'Amount has to be greater than 0'
-        transaction = {'sender':sender, 'recipient':recipient, 'subject':subject, 'amount':amount}
+        assert amount > 0, 'Amount has to be greater than 0'
+        transaction = {'sender': sender, 'recipient': recipient, 'subject': subject, 'amount': amount}
         self.transactions.append(transaction)
         return transaction
-
