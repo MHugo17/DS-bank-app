8e69b4af5a905033e5179c4c4ea95d5de90113ea
Martin Hugo <hugo0811@gmail.com>
2019-02-07 18:02:20 +0100

    date time and id

--- src/app/transaction.py
+++ src/app/transaction.py
@@ -1,6 +1,8 @@
+import datetime
+import uuid
 class Transaction:
     #init class with arguments of the transaction
-    def __init__(self, *, sender, recipient, subject, amount):
+    def __init__(self, id=None, time = None, *, sender, recipient, subject, amount):
         # determina that sender and recipient must be integer, amount must be a float
         assert type(sender) == int, 'Sender needs to be an integer'
         assert type(recipient) == int, 'Recipient needs to be an integer'
@@ -10,7 +12,10 @@ class Transaction:
         self.recipient = recipient
         self.subject = subject
         self.amount = amount
-
+        #date and time of transaction
+        self.time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        #number id of transaction
+        self.id = uuid.uuid4()
     #extract transaction info
     def info(self):
-        return(f'From {self.sender} to {self.recipient}: {self.subject} - {self.amount} €')
+        return(f'From {self.sender} to {self.recipient}: {self.subject} - {self.amount} € {self.time} {self.id}')
